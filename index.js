9db2f992a80bc2e153d9e7c47bde2d06ea83ffdc
U-SJC_PC\SJC <redsox333@gmail.com>
2013-04-01 18:49:03 -0700

    Added better commenting and description in node server files

--- index.js
+++ index.js
@@ -1,21 +1,72 @@
-// initial values
+/*
+	Node Server
+	
+	This is the main file for running the NodeJS server to host the application.  
+	It initiates the app using the Express Framework for Node, sets the static file path for the app,
+	handles the routing by calling specific functions included in the node/handlers.js file, and listens
+	on a specified port.
+	
+	Contents:
+	1. Set Initial Values
+	2. Include Dependencies
+	3. Start App With Express
+	4. Configuration
+		4.1 Set Static File Path
+	5. Routing
+	6. Listen on Specified Port
+	
+	
+	To add a route:
+		**Note** All api calls should have 'api/' in front of them to avoid conflicts with the angular routing.
+		
+		1. Define a new app.get or app.post in section 5.
+			ex: app.get('api/myNewRoute', handlers.myNewRoute)
+		2. Define a new handler function in the node/handlers.js file
+			ex (in node/handlers.js):
+			
+			function myNewRoute(request, response) {
+				response.send('this is my new route!');
+			}
+			exports.myNewRoute = myNewRoute;
+*/
+
+
+/*
+	1. Set Initial Values
+*/
 var port = 1337;
 
-// include dependencies
+
+/*
+	2. Include Dependencies
+*/
 var express = require('express');
 var handlers = require('./node/handlers');
 
-// start app
+
+/*
+	3. Start App With Express
+*/
 var app = express();
 
-// configuration
-// set static file path
+
+/* 
+	4. Configuration
+*/
+
+// 4.1 Set Static File Path
 app.use(express.static(__dirname + '/app'));
 
-// routing
+
+/*
+	5. Routing
+*/
 app.get('/api/goodbye', handlers.bye);
 app.get('*', handlers.index);
 
-// listen on port specified
+
+/*
+	6. Listen On Specified Port
+*/
 app.listen(port);
 console.log('Listening on port %d.', port);
